821e0e473591d7e9dc24c05c9d781980a73b4773
Luke <as2689133@gmail.com>
2024-05-30 12:33:40 +0800

    更新

--- basic_module/test_CQ9.py
+++ basic_module/test_CQ9.py
@@ -9,28 +9,42 @@ async def test_specific_feature():
     playwright, browser, context = await setup_browser()  # 使用默认设备iPhone 11
     try:
         page = await load_and_check_page(context)
-        print("Page loaded successfully.")
+        print("页面加载成功。")
 
         await login(page, "all24042501", "396012")  # 使用公共登录函数
 
         with step("点击娱乐城"):
             await page.click("div.label:has-text('娛樂城')")
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
             await take_screenshot_and_attach(page, "点击娱乐城后")
 
+        with step("关闭导览页"):
+            await page.click("svg[width='24px'][height='24px'][viewBox='0 0 1024 1024'] path[fill='#999']")
+            await asyncio.sleep(2)
+            await take_screenshot_and_attach(page, "关闭导览页后")
+
+        with step("关闭导览页2"):
+            await page.click("svg[width='24px'][height='24px'][viewBox='0 0 1024 1024'] path[fill='#999']")
+            await asyncio.sleep(2)
+            await take_screenshot_and_attach(page, "关闭导览页后")
+
         with step("关闭公告"):
-            await page.click("button.promotionAd-btn.show")
+            viewport_size = page.viewport_size
+            # 点击屏幕中心位置
+            click_x = viewport_size['width'] * 0.5
+            click_y = viewport_size['height'] * 0.10
+            await page.mouse.click(click_x, click_y)
             await asyncio.sleep(2)
             await take_screenshot_and_attach(page, "公告关闭后")
 
         with step("选择CQ9电子游戏"):
             await page.click("div.enter-game-button.bk-cq9-slot.zh-TW")
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
             await take_screenshot_and_attach(page, "选择CQ9电子游戏后")
 
         with step("点击水果派对游戏图标"):
             await page.click("img[alt='水果派對']")
-            await asyncio.sleep(11)
+            await asyncio.sleep(20)
             await take_screenshot_and_attach(page, "点击水果派对游戏图标后")
 
         with step("在Canvas上进行点击"):
@@ -38,14 +52,14 @@ async def test_specific_feature():
             click_x = viewport_size['width'] * 0.10
             click_y = viewport_size['height'] * 0.76
             await page.mouse.click(click_x, click_y)
-            print(f"Clicked at X: {click_x}, Y: {click_y}")
-            await asyncio.sleep(3.5)  # 等待可能的页面响应或跳转
+            print(f"在 X: {click_x}, Y: {click_y} 位置点击")
+            await asyncio.sleep(5)  # 等待可能的页面响应或跳转
             await take_screenshot_and_attach(page, "在Canvas进行点击后")
 
-        print("Test execution completed.")
+        print("测试执行完毕。")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"发生错误: {e}")
     finally:
         if browser:
             await browser.close()
